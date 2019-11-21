e532a1c0a2a11ace90f7f0c0bfff9f427b781dee
JamesDevon <51328809+JamesDevon@users.noreply.github.com>
2019-11-21 19:40:36 +0200

    API update
    Added example of json files we are getting from the api. Also added an example python script on how to communicate with the api.

---
+++ Data/Dataset/accessApiScript.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 19:23:42 2019
+
+@author: James
+"""
+import json
+import requests
+
+response = requests.get(
+    url="https://api.predicthq.com/v1/events",
+    headers={
+      "Authorization": "Bearer oPZoBwG7_7dQQm7KC_s3MmhqxVu_4805awvIuI8D",
+      "Accept": "application/json"
+    },
+    params={
+      "id": "WZxdyFuXzcuGc24oBv"
+    }
+)
+
+with open("singleEventExample.json","w") as jsonFile:
+    json.dump(response.json(),jsonFile)
+    
+response = requests.get(
+    url="https://api.predicthq.com/v1/events",
+    headers={
+      "Authorization": "Bearer oPZoBwG7_7dQQm7KC_s3MmhqxVu_4805awvIuI8D",
+      "Accept": "application/json"
+    },
+    params={
+      "country": "GR"
+    }
+)
+
+with open("EventsExample.json","w") as jsonFile:
+    json.dump(response.json(),jsonFile)
