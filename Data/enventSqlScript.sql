883a50ca37ba3f2fbeccac06a31e644d7a50fa2e
JamesDevon <51328809+JamesDevon@users.noreply.github.com>
2019-12-02 15:43:04 +0200

    Database Update

--- Data/enventSqlScript.sql
+++ Data/enventSqlScript.sql
@@ -15,96 +15,100 @@ drop table `labeled`;
 SET foreign_key_checks = 1;
 
 CREATE TABLE `user` (
-  `id` int not null PRIMARY KEY AUTO_INCREMENT,
-  `fname` varchar(255),
-  `lname` varchar(255),
-  `username` varchar(255) not null,
-  `password` varchar(255) not null,
-  `interests` varchar(255),
-  `email` varchar(255),
-  `address` varchar(255),
-  `created_at` timestamp,
-  `last_login` timestamp
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(255) DEFAULT NULL,
+  `lname` varchar(255) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `interests` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `events` (
-  `id` int not null PRIMARY KEY AUTO_INCREMENT,
-  `title` varchar(255),
-  `category_id` int,
-  `created_at` timestamp,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `description` text,
-  `participants_amount` int,
-  `admin_id` int,
-  `json_id` varchar(255)
-);
+  `participants_amount` int(11) DEFAULT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `admin_id` (`admin_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `events_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`),
+  CONSTRAINT `events_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `category` (
-  `id` int not null PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255)
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `father` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `venues` (
-  `id` int not null PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255),
-  `location` varchar(255),
-  `capacity` int,
-  `json_id` varchar(255)
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8
 
 CREATE TABLE `admin` (
-  `id` int not null PRIMARY KEY AUTO_INCREMENT,
-  `fname` varchar(255),
-  `lname` varchar(255),
-  `username` varchar(255),
-  `password` varchar(255),
-  `iban` varchar(255),
-  `email` varchar(255),
-  `address` varchar(255)
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(255) DEFAULT NULL,
+  `lname` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `iban` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `reservations` (
-  `id` int not null PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int,
-  `event_id` int,
-  `bool` bool,
-  `payment_type` varchar(255)
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `event_id` int(11) DEFAULT NULL,
+  `bool` tinyint(1) DEFAULT NULL,
+  `payment_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `event_id` (`event_id`),
+  CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `play` (
-  `event_id` int,
-  `venue_id` int,
-  `date` timestamp,
-  primary key (`event_id`, `date`)
-);
+  `event_id` int(11) NOT NULL,
+  `venue_id` int(11) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`event_id`,`date`),
+  KEY `venue_id` (`venue_id`),
+  CONSTRAINT `play_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`),
+  CONSTRAINT `play_ibfk_2` FOREIGN KEY (`venue_id`) REFERENCES `venues` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `labels`(
-`id` int not null primary key auto_increment,
-`name` varchar(255)
-);
+CREATE TABLE `labels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `labeled`(
-`event_id` int,
-`label_id` int,
-primary key (`event_id`,`label_id`)
-);
-
-ALTER TABLE `events` ADD FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`);
-
-ALTER TABLE `reservations` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `reservations` ADD FOREIGN KEY (`event_id`) REFERENCES `events` (`id`);
-
-ALTER TABLE `events` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
-
-ALTER TABLE `play` ADD FOREIGN KEY (`event_id`) REFERENCES `events` (`id`);
-
-ALTER TABLE `play` ADD FOREIGN KEY (`venue_id`) REFERENCES `venues` (`id`);
-
-ALTER TABLE `category` ADD column father int after name;
-
-ALTER TABLE `labeled` ADD FOREIGN KEY (`event_id`) REFERENCES `events` (`id`);
-
-ALTER TABLE `labeled` ADD FOREIGN KEY (`label_id`) REFERENCES `labels` (`id`);
+CREATE TABLE `labeled` (
+  `event_id` int(11) NOT NULL,
+  `label_id` int(11) NOT NULL,
+  PRIMARY KEY (`event_id`,`label_id`),
+  KEY `label_id` (`label_id`),
+  CONSTRAINT `labeled_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`),
+  CONSTRAINT `labeled_ibfk_2` FOREIGN KEY (`label_id`) REFERENCES `labels` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 insert into category(name,father) values ('Music',null);
 insert into category(name,father) values ('Theater',null);
