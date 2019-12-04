57b30ed578ecae32bf8275b29ad2669642578932
JamesDevon <51328809+JamesDevon@users.noreply.github.com>
2019-12-04 21:25:22 +0200

    Added Some Classes

---
+++ Envent/Events_Omada1/Project 4/User Classes/Admin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_4.User_Classes
+{
+    class Admin : User
+    {
+        private List<RegisteredUser> Users;
+        private string userName;
+        private string passWord;
+        private string userID;
+
+        public void editProfile(Profile profile)
+        {
+
+        }
+
+        public void deleteProfile(Profile profile)
+        {
+
+        }
+
+        public void editEvents(Event events)
+        {
+
+        }
+    }
+}
---
+++ Envent/Events_Omada1/Project 4/User Classes/Visitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//author Τζέιμς Μπαλάση
+namespace Project_4.User_Classes
+{
+    class Visitor
+    {
+
+        public void registerUser(Profile profile)
+        {
+
+        }
+
+        public RegisteredUser logIn(string userName,string passWord)
+        {
+            RegisteredUser test = new RegisteredUser();
+            return test;
+        }
+
+    }
+}
