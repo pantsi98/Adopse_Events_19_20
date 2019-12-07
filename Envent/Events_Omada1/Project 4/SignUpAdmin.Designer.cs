37f987da557f36f80558cdb90b71383ed84f4aff
stellakaranikola <44724193+stellakaranikola@users.noreply.github.com>
2019-12-07 15:46:24 +0200

    merge with develop

---
+++ Envent/Events_Omada1/Project 4/SignUpAdmin.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Project_4
+{
+    partial class SignUpAdmin
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Title = new System.Windows.Forms.Label();
+            this.IBAN = new System.Windows.Forms.TextBox();
+            this.Email1 = new System.Windows.Forms.TextBox();
+            this.username1 = new System.Windows.Forms.TextBox();
+            this.messagefullo = new System.Windows.Forms.Label();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.Register = new System.Windows.Forms.Button();
+            this.Kodikos1 = new System.Windows.Forms.TextBox();
+            this.Kodikos2 = new System.Windows.Forms.TextBox();
+            this.Epitheto = new System.Windows.Forms.TextBox();
+            this.Onoma = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.IBAN);
+            this.panel2.Controls.Add(this.Email1);
+            this.panel2.Controls.Add(this.username1);
+            this.panel2.Controls.Add(this.messagefullo);
+            this.panel2.Controls.Add(this.messageLabel);
+            this.panel2.Controls.Add(this.Register);
+            this.panel2.Controls.Add(this.Kodikos1);
+            this.panel2.Controls.Add(this.Kodikos2);
+            this.panel2.Controls.Add(this.Epitheto);
+            this.panel2.Controls.Add(this.Onoma);
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.20895F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.panel2.Location = new System.Drawing.Point(0, -38);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1130, 745);
+            this.panel2.TabIndex = 14;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(128)))), ((int)(((byte)(233)))));
+            this.panel1.Controls.Add(this.Title);
+            this.panel1.Location = new System.Drawing.Point(0, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1130, 63);
+            this.panel1.TabIndex = 30;
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Title.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Title.Location = new System.Drawing.Point(270, 16);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(589, 31);
+            this.Title.TabIndex = 15;
+            this.Title.Text = "Δημιουργήστε Nέο Λογαριασμό Διοργανωτή";
+            // 
+            // IBAN
+            // 
+            this.IBAN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.20895F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.IBAN.ForeColor = System.Drawing.Color.Gray;
+            this.IBAN.Location = new System.Drawing.Point(328, 416);
+            this.IBAN.Name = "IBAN";
+            this.IBAN.Size = new System.Drawing.Size(472, 27);
+            this.IBAN.TabIndex = 29;
+            this.IBAN.Text = "IBAN";
+            this.IBAN.MouseClick += new System.Windows.Forms.MouseEventHandler(this.IBAN_MouseClick);
+            this.IBAN.Leave += new System.EventHandler(this.IBAN_Leave);
+            // 
+            // Email1
+            // 
+            this.Email1.ForeColor = System.Drawing.Color.Gray;
+            this.Email1.Location = new System.Drawing.Point(328, 317);
+            this.Email1.Name = "Email1";
+            this.Email1.Size = new System.Drawing.Size(473, 27);
+            this.Email1.TabIndex = 28;
+            this.Email1.Text = "Email";
+            this.Email1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Email1_MouseClick);
+            this.Email1.Leave += new System.EventHandler(this.Email1_Leave);
+            // 
+            // username1
+            // 
+            this.username1.ForeColor = System.Drawing.Color.Gray;
+            this.username1.Location = new System.Drawing.Point(328, 284);
+            this.username1.Name = "username1";
+            this.username1.Size = new System.Drawing.Size(473, 27);
+            this.username1.TabIndex = 27;
+            this.username1.Text = "Username";
+            this.username1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.username1_MouseClick);
+            this.username1.Leave += new System.EventHandler(this.username1_Leave);
+            // 
+            // messagefullo
+            // 
+            this.messagefullo.AutoSize = true;
+            this.messagefullo.Location = new System.Drawing.Point(180, 326);
+            this.messagefullo.Name = "messagefullo";
+            this.messagefullo.Size = new System.Drawing.Size(0, 22);
+            this.messagefullo.TabIndex = 23;
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.134328F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.messageLabel.Location = new System.Drawing.Point(324, 228);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(111, 20);
+            this.messageLabel.TabIndex = 13;
+            this.messageLabel.Text = "message label";
+            this.messageLabel.Visible = false;
+            // 
+            // Register
+            // 
+            this.Register.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(128)))), ((int)(((byte)(233)))));
+            this.Register.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Register.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Register.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Register.Location = new System.Drawing.Point(470, 501);
+            this.Register.Name = "Register";
+            this.Register.Size = new System.Drawing.Size(189, 51);
+            this.Register.TabIndex = 10;
+            this.Register.Text = "Eγγραφή";
+            this.Register.UseVisualStyleBackColor = false;
+            this.Register.Click += new System.EventHandler(this.Register_Click);
+            // 
+            // Kodikos1
+            // 
+            this.Kodikos1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.20895F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.Kodikos1.ForeColor = System.Drawing.Color.Gray;
+            this.Kodikos1.Location = new System.Drawing.Point(328, 350);
+            this.Kodikos1.Name = "Kodikos1";
+            this.Kodikos1.Size = new System.Drawing.Size(472, 27);
+            this.Kodikos1.TabIndex = 8;
+            this.Kodikos1.Text = "Κωδικός";
+            this.Kodikos1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Kodikos1_MouseClick);
+            this.Kodikos1.Leave += new System.EventHandler(this.Kodikos1_Leave);
+            // 
+            // Kodikos2
+            // 
+            this.Kodikos2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.20895F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.Kodikos2.ForeColor = System.Drawing.Color.Gray;
+            this.Kodikos2.Location = new System.Drawing.Point(328, 383);
+            this.Kodikos2.Name = "Kodikos2";
+            this.Kodikos2.Size = new System.Drawing.Size(472, 27);
+            this.Kodikos2.TabIndex = 9;
+            this.Kodikos2.Text = "Επαλήθευση Κωδικού";
+            this.Kodikos2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Kodikos2_MouseClick);
+            this.Kodikos2.Leave += new System.EventHandler(this.Kodikos2_Leave);
+            // 
+            // Epitheto
+            // 
+            this.Epitheto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.20895F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.Epitheto.ForeColor = System.Drawing.Color.Gray;
+            this.Epitheto.Location = new System.Drawing.Point(567, 251);
+            this.Epitheto.Name = "Epitheto";
+            this.Epitheto.Size = new System.Drawing.Size(233, 27);
+            this.Epitheto.TabIndex = 6;
+            this.Epitheto.Text = "Επώνυμο";
+            this.Epitheto.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Epitheto_MouseClick);
+            this.Epitheto.Leave += new System.EventHandler(this.Epitheto_Leave);
+            // 
+            // Onoma
+            // 
+            this.Onoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.20895F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.Onoma.ForeColor = System.Drawing.Color.DimGray;
+            this.Onoma.Location = new System.Drawing.Point(328, 251);
+            this.Onoma.Name = "Onoma";
+            this.Onoma.Size = new System.Drawing.Size(233, 27);
+            this.Onoma.TabIndex = 5;
+            this.Onoma.Text = "Όνομα";
+            this.Onoma.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Onoma_MouseClick);
+            this.Onoma.Leave += new System.EventHandler(this.Onoma_Leave);
+            // 
+            // SignUpAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Name = "SignUpAdmin";
+            this.Size = new System.Drawing.Size(1130, 745);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox Email1;
+        private System.Windows.Forms.TextBox username1;
+        private System.Windows.Forms.Label messagefullo;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Button Register;
+        private System.Windows.Forms.TextBox Kodikos1;
+        private System.Windows.Forms.TextBox Kodikos2;
+        private System.Windows.Forms.TextBox Epitheto;
+        private System.Windows.Forms.TextBox Onoma;
+        private System.Windows.Forms.TextBox IBAN;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
