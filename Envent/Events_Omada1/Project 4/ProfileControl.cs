63320e8ad452fd700f25897cbb56e4e066a61984
dimosthenisgr <48571367+dimostenisgr@users.noreply.github.com>
2019-12-21 17:17:20 +0200

    User Profile v3

--- Envent/Events_Omada1/Project 4/ProfileControl.cs
+++ Envent/Events_Omada1/Project 4/ProfileControl.cs
@@ -36,5 +36,30 @@ namespace Project_4
         {
 
         }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dobPicker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
