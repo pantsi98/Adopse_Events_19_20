4dc890bd615b5a43eba7d7a3a2f02eaf7fce3084
JamesDevon <51328809+JamesDevon@users.noreply.github.com>
2019-12-25 18:07:36 +0200

    Update

--- Envent/Events_Omada1/Project 4/HomeMain.cs
+++ Envent/Events_Omada1/Project 4/HomeMain.cs
@@ -21,32 +21,38 @@ namespace Project_4
         private void HomeMain_Load(object sender, EventArgs e)
         {
 
-             int index = 0;
-             User user = InstanceOfUser.GetUser();
-             List<Event> events = new List<Event>();
-            foreach(Event ev in user.ShowEvents())
+            int index = 0;
+            User user = InstanceOfUser.GetUser();
+            List<Event> events = App_Code.StaticMethods.Events.events;
+            List<Categories> cat = Categories.categories;
+            foreach (Control p in musicPanel.Controls)
             {
-                if(ev.GetCategory())
+                foreach (Control i in p.Controls)
+                {
+                    if (i is PictureBox)
+                    {
+                        PictureBox pic = (PictureBox)i;
+                        pic.Image = Images.pic.ElementAt(index);
+                    }
+                    if (i is Label)
+                    {
+                        Label lb = (Label)i;
+                        lb.Text = events.ElementAt(index).GetTitle();
+                    }
+                }
+                index++;
             }
 
-             List<Categories> cat = Categories.categories;
-             foreach (Control p in musicPanel.Controls)
-             {
-                 foreach (Control i in p.Controls)
-                 {
-                     if (i is PictureBox)
-                     {
-                         PictureBox pic = (PictureBox)i;
-                         pic.Image = Images.pic.ElementAt(index);
-                     }
-                     if (i is Label)
-                     {
-                         Label lb = (Label)i;
-                         lb.Text = events.ElementAt(index).GetTitle();
-                     }
-                     index++;
-                 }
-             }
+            foreach(Control i in flowLayoutPanel1.Controls)
+            {
+                foreach(Control p in i.Controls)
+                {
+                    foreach(Control k in p.Controls)
+                    {
+
+                    }
+                }
+            }
         }
 
         private void label17_Click(object sender, EventArgs e)
--- Envent/Events_Omada1/Project 4/Program.cs
+++ Envent/Events_Omada1/Project 4/Program.cs
@@ -20,15 +20,9 @@ namespace Project_4
         [STAThread]
         static void Main()
         {
-            Events.FillEventsData();
-            WebClient cli = new WebClient();
-            List<string> imgUrls = new List<string>();
-            for(int i=0; i<Events.events.Count; i++)
-            {
-                imgUrls.Add(Events.events.ElementAt(i).GetImage());
-                cli.DownloadFile(imgUrls.ElementAt(i), @"C:\Users\dimosthenisgr\Documents\GitHub\Adopse_Events_19_20\Envent\Events_Omada1\Project 4\Resources\eventImg"+i+".jpg");
-                Images.pic.Add(Image.FromFile(@"C:\Users\dimosthenisgr\Documents\GitHub\Adopse_Events_19_20\Envent\Events_Omada1\Project 4\Resources\eventImg"+i+".jpg"));
-            }
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            d.Add(1, "Images.LoadImages");
+            d[1]();
             InstanceOfUser.CreateVisitor();
             Categories.FillCategoryData();
             Application.EnableVisualStyles();
