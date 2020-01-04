10afb662e16466c6ada097f4c9a3684bffd9187f
stellakaranikola <44724193+stellakaranikola@users.noreply.github.com>
2020-01-04 12:50:58 +0200

    details create event

--- Envent/Events_Omada1/Project 4/CreateNewEvent.cs
+++ Envent/Events_Omada1/Project 4/CreateNewEvent.cs
@@ -19,14 +19,11 @@ namespace Project_4
         public CreateNewEvent()
         {
             InitializeComponent();
+          
         }
 
         public int Id;
-       
-
-
-
-
+      
 
         private void CreateNewEvent_Load(object sender, EventArgs e)
         {
@@ -62,14 +59,9 @@ namespace Project_4
                 deiktislathwn = false;
             else if (durationhour.Value == 0 || durationmin.Value == 0)
                 deiktislathwn = false;
-            else if (topothesia.Text == "")
+            else if (katigoria.Text == "")
                 deiktislathwn = false;
-            else if (kanonikoticket.Value == 0 || meiomenoticket.Value == 0)
-                deiktislathwn = false;
-            else if (timehour.Value == 0 || Timemin.Value == 0)
-                deiktislathwn = false;
-
-
+            
             return deiktislathwn;
         }
 
@@ -84,6 +76,7 @@ namespace Project_4
                 createEvent.Enabled = true;
             }
         }
+
         int kat;
         //methodos gia na dinei to swsto id stin katigoria
         private int katigoriacomboboxselect()
@@ -126,7 +119,137 @@ namespace Project_4
 
             EventManager em = (EventManager)uem;
             em.CreateEvent(titlos.Text, katigoriacomboboxselect(), perigrafi.Text, selectedduration());
+            //edw apotelesma pou an gurizei id event
+
+            //enable ta textbox 
+            titlos.Enabled = true;
+            katigoria.Enabled = true;
+            durationhour.Enabled = true;
+            durationmin.Enabled = true;
+            uploadImage.Enabled = true;
+            createEvent.Enabled = true;
+
+
             
+        }
+
+        private void createplaypanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Addplay_Click(object sender, EventArgs e)
+        {
+            int normalticket = Convert.ToInt32(kanonikoticket.Value);
+            int reducedticket = Convert.ToInt32(meiomenoticket.Value);
+            //dokimastiko id play
+            int play_id=0;
+            Ticket tic = new Ticket();
+            tic.CreateTicket("normal", normalticket, play_id);
+            tic.CreateTicket("reduced", reducedticket, play_id);
+
+
+
+
+            ///entoles gia na emfanizei to play poy egine
+            String date= dateofPlay.Value.ToString("yyyy-MM-dd");
+            String time = timepicker.Value.ToString();
+            String kan = kanonikoticket.Value.ToString();
+            String meiom = meiomenoticket.Value.ToString();
+            ListViewItem item = new ListViewItem(date);
+            item.SubItems.Add(time);
+            item.SubItems.Add(topothesia.Text);
+            item.SubItems.Add(kan);
+            item.SubItems.Add(meiom);
+
+        }
+
+        private void kanonikoticket_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        Boolean tit = false;
+        Boolean katig = false;
+        Boolean per = false;
+        Boolean dur = false;
+
+        private void circularPicture5_Click(object sender, EventArgs e)
+        { 
+            createEvent.Visible = false;
+            savechanges.Visible = true;
+            titlos.Enabled = false;
+            tit = true;
+        }
+
+        private void circularPicture1_Click(object sender, EventArgs e)
+        {
+            createEvent.Visible = false;
+            savechanges.Visible = true;
+            katigoria.Enabled = false;
+            katig = true;
+        }
+
+        private void circularPicture4_Click(object sender, EventArgs e)
+        {
+            createEvent.Visible = false;
+            savechanges.Visible = true;
+            perigrafi.Enabled = false;
+            per = true;
+        }
+
+        private void circularPicture2_Click(object sender, EventArgs e)
+        {
+            createEvent.Visible = false;
+            savechanges.Visible = true;
+            durationhour.Enabled = false;
+            durationmin.Enabled = false;
+            dur = true;
+        }
+
+        private void circularPicture3_Click(object sender, EventArgs e)
+        {
+
+            createEvent.Visible = false;
+            savechanges.Visible = true;
+            uploadImage.Enabled = false;
+
+            
+        }
+
+        private void savechanges_Click(object sender, EventArgs e)
+        {
+            EventManager em = (EventManager)uem;
+            
+            if (tit = true)
+            {
+                em.UpdateEventTitle(1, titlos.Text);
+            }
+            else if (katig = true)
+            {
+                em.UpdateEventCategory(1, katigoriacomboboxselect());
+            }
+            else if (per = true)
+            {
+                em.UpdateEventDescription(1, perigrafi.Text);
+
+            }
+            else if (dur = true) {
+                em.UpdateEventDuration(1, selectedduration());
+            }
+            tit = false;
+            katig = false;
+            per = false;
+            dur = false;
+            savechanges.Visible = false;
+            createEvent.Visible = true;
+            createEvent.Enabled = false;
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
