ef098e91df721fe21eac22aada48f4b052450425
pelpanagiotis <pelpanagiotis@gmail.com>
2020-01-11 12:01:17 +0200

    Revert "Merge branch 'Final_Fixes' of https://github.com/PanChee00/Adopse_Events_19_20 into Final_Fixes"
    This reverts commit fe18a161baf5729fbcfa5f57bc079c8633808665, reversing
    changes made to b40860af183fc66b360e0e5a93f88c6789de1fad.

--- Envent/Events_Omada1/Project 4/App_Code/Visitor.cs
+++ Envent/Events_Omada1/Project 4/App_Code/Visitor.cs
@@ -91,8 +91,7 @@ namespace Project_4.User_Classes
                     return InstanceOfUser.GetUser();
 
                 } else {
-                // throw new FailLoginAsEventManager("O manager δεν υπάρχει");
-                throw new FailLoginAsEventManager();
+                    throw new FailLoginAsEventManager("O manager δεν υπάρχει");
                 }
             }
 
