73499b0ed32575c22c9e0036459d92ffe878acd7
pelpanagiotis <pelpanagiotis@gmail.com>
2020-01-11 12:05:27 +0200

    Revert "Revert "Merge branch 'Final_Fixes' of https://github.com/PanChee00/Adopse_Events_19_20 into Final_Fixes""
    This reverts commit ef098e91df721fe21eac22aada48f4b052450425.

--- Envent/Events_Omada1/Project 4/LogIn.Designer.cs
+++ Envent/Events_Omada1/Project 4/LogIn.Designer.cs
@@ -53,8 +53,9 @@
             this.LogInpanel.Controls.Add(this.PasswordLog);
             this.LogInpanel.Controls.Add(this.UsernameLog);
             this.LogInpanel.Location = new System.Drawing.Point(0, 0);
+            this.LogInpanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.LogInpanel.Name = "LogInpanel";
-            this.LogInpanel.Size = new System.Drawing.Size(1130, 745);
+            this.LogInpanel.Size = new System.Drawing.Size(848, 605);
             this.LogInpanel.TabIndex = 0;
             this.LogInpanel.Paint += new System.Windows.Forms.PaintEventHandler(this.LogInpanel_Paint);
             this.LogInpanel.MouseHover += new System.EventHandler(this.LogInpanel_MouseHover);
@@ -62,17 +63,19 @@
             // msglabel
             // 
             this.msglabel.AutoSize = true;
-            this.msglabel.Location = new System.Drawing.Point(540, 104);
+            this.msglabel.Location = new System.Drawing.Point(405, 84);
+            this.msglabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.msglabel.Name = "msglabel";
-            this.msglabel.Size = new System.Drawing.Size(0, 17);
+            this.msglabel.Size = new System.Drawing.Size(0, 13);
             this.msglabel.TabIndex = 7;
             // 
             // messagelabel
             // 
             this.messagelabel.AutoSize = true;
-            this.messagelabel.Location = new System.Drawing.Point(255, 647);
+            this.messagelabel.Location = new System.Drawing.Point(191, 526);
+            this.messagelabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.messagelabel.Name = "messagelabel";
-            this.messagelabel.Size = new System.Drawing.Size(0, 17);
+            this.messagelabel.Size = new System.Drawing.Size(0, 13);
             this.messagelabel.TabIndex = 6;
             // 
             // loginfail
@@ -80,9 +83,10 @@
             this.loginfail.AutoSize = true;
             this.loginfail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.loginfail.ForeColor = System.Drawing.Color.Red;
-            this.loginfail.Location = new System.Drawing.Point(254, 129);
+            this.loginfail.Location = new System.Drawing.Point(190, 105);
+            this.loginfail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.loginfail.Name = "loginfail";
-            this.loginfail.Size = new System.Drawing.Size(637, 20);
+            this.loginfail.Size = new System.Drawing.Size(546, 17);
             this.loginfail.TabIndex = 5;
             this.loginfail.Text = "Ο κωδικός πρόσβασης ή το Email που καταχωρήσατε είναι λάθος. Ξαναπροσπαθήστε.";
             this.loginfail.Visible = false;
@@ -90,9 +94,10 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(537, 471);
+            this.linkLabel1.Location = new System.Drawing.Point(403, 383);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(196, 17);
+            this.linkLabel1.Size = new System.Drawing.Size(162, 13);
             this.linkLabel1.TabIndex = 4;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Ξεχάσατε το λογαριασμό σας;";
@@ -104,9 +109,10 @@
             this.Syndesi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Syndesi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
             this.Syndesi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Syndesi.Location = new System.Drawing.Point(475, 344);
+            this.Syndesi.Location = new System.Drawing.Point(356, 280);
+            this.Syndesi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Syndesi.Name = "Syndesi";
-            this.Syndesi.Size = new System.Drawing.Size(180, 46);
+            this.Syndesi.Size = new System.Drawing.Size(135, 37);
             this.Syndesi.TabIndex = 3;
             this.Syndesi.Text = "Σύνδεση";
             this.Syndesi.UseVisualStyleBackColor = false;
@@ -116,21 +122,24 @@
             // 
             this.PasswordLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.PasswordLog.ForeColor = System.Drawing.Color.Gray;
-            this.PasswordLog.Location = new System.Drawing.Point(388, 255);
+            this.PasswordLog.Location = new System.Drawing.Point(291, 207);
+            this.PasswordLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PasswordLog.Name = "PasswordLog";
-            this.PasswordLog.Size = new System.Drawing.Size(345, 34);
+            this.PasswordLog.Size = new System.Drawing.Size(260, 28);
             this.PasswordLog.TabIndex = 1;
             this.PasswordLog.Text = "Κωδικός";
             this.PasswordLog.Click += new System.EventHandler(this.PasswordLog_Click);
+            this.PasswordLog.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PasswordLog_KeyPress);
             this.PasswordLog.Leave += new System.EventHandler(this.PasswordLog_Leave);
             // 
             // UsernameLog
             // 
             this.UsernameLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.UsernameLog.ForeColor = System.Drawing.Color.Gray;
-            this.UsernameLog.Location = new System.Drawing.Point(388, 189);
+            this.UsernameLog.Location = new System.Drawing.Point(291, 154);
+            this.UsernameLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.UsernameLog.Name = "UsernameLog";
-            this.UsernameLog.Size = new System.Drawing.Size(345, 34);
+            this.UsernameLog.Size = new System.Drawing.Size(260, 28);
             this.UsernameLog.TabIndex = 0;
             this.UsernameLog.Text = "Username";
             this.UsernameLog.Click += new System.EventHandler(this.UsernameLog_Click);
@@ -143,9 +152,10 @@
             this.label.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
             this.label.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label.Location = new System.Drawing.Point(505, 14);
+            this.label.Location = new System.Drawing.Point(379, 11);
+            this.label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(120, 31);
+            this.label.Size = new System.Drawing.Size(97, 25);
             this.label.TabIndex = 2;
             this.label.Text = "Είσοδος";
             // 
@@ -154,18 +164,20 @@
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(128)))), ((int)(((byte)(233)))));
             this.panel1.Controls.Add(this.label);
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1130, 63);
+            this.panel1.Size = new System.Drawing.Size(848, 51);
             this.panel1.TabIndex = 31;
             // 
             // LogIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.LogInpanel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LogIn";
-            this.Size = new System.Drawing.Size(1130, 745);
+            this.Size = new System.Drawing.Size(848, 605);
             this.Load += new System.EventHandler(this.LogIn_Load_1);
             this.LogInpanel.ResumeLayout(false);
             this.LogInpanel.PerformLayout();
