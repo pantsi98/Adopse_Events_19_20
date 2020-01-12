38ecbc783a12ee834082cdebd8d371e0bf049fff
JamesDevon <jbalashi@gmail.com>
2020-01-12 17:53:15 +0000

    Play.cs update

--- Envent/Events_Omada1/Project 4/App_Code/Play.cs
+++ Envent/Events_Omada1/Project 4/App_Code/Play.cs
@@ -10,13 +10,17 @@ namespace Project_4.App_Code
 {
     public class Play
     {
-        private Event ev;
-        private Venue venue;
+        private Event ev1;
+        private Venue venue1;
         private static List<DateTime> dates;
         private int ev;
         private int venue;
         private DateTime date;
         private bool status;
+
+        static enventDataSetTableAdapters.playTableAdapter pla = new enventDataSetTableAdapters.playTableAdapter();
+        static List<enventDataSet.playRow> pl = new List<enventDataSet.playRow>();
+        public static List<Play> plays = new List<Play>();
 
         public Play(int eventID) {
             dates = new List<DateTime>();
@@ -30,10 +34,44 @@ namespace Project_4.App_Code
             }
         }
 
+        public Play(int ev, int venue, DateTime date, bool status)
+        {
+            this.ev = ev;
+            this.venue = venue;
+            this.date = date;
+            this.status = status;
+        }
+
+
+        public static void FillPlaysData()
+        {
+            pl = pla.GetPlays().ToList();
+            for (int i = 0; i < pl.Count; i++)
+            {
+                Play playItem = new Play(pl.ElementAt(i).event_id, pl.ElementAt(i).venue_id, pl.ElementAt(i).date, pl.ElementAt(i).status);
+                plays.Add(playItem);
+            }
+        }
+
         public List<DateTime> GetDates()
         {
 
             return dates;
+        }
+
+        public DateTime getDates()
+        {
+            return this.date;
+        }
+
+        public int GetEventID()
+        {
+            return this.ev;
+        }
+
+        public int GetVenueID()
+        {
+            return this.venue;
         }
     }
 }
